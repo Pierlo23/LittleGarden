6a078e570aecc6364bd21318d03d26d4b6c0440f
pierlo23 <pierluigilangellotti@outlook.com>
2026-05-19 23:02:04 +0200

    aggiunti primi bean

---
+++ src/main/java/model/Prodotto.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.io.Serializable;
+
+public class Prodotto implements Serializable{
+	private int idProdotto;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private String immagineUrl;
+    private boolean isCancellato;
+	
+    public Prodotto () {
+    	
+    }
+    
+
+	public Prodotto(int idProdotto, String nome, String descrizione, double prezzo, String immagineUrl,
+			boolean isCancellato) {
+		super();
+		this.idProdotto = idProdotto;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.immagineUrl = immagineUrl;
+		this.isCancellato = isCancellato;
+	}
+    
+    
+    public int getIdProdotto() {
+		return idProdotto;
+	}
+
+
+
+	public void setIdProdotto(int idProdotto) {
+		this.idProdotto = idProdotto;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+
+
+	public String getImmagineUrl() {
+		return immagineUrl;
+	}
+
+
+
+	public void setImmagineUrl(String immagineUrl) {
+		this.immagineUrl = immagineUrl;
+	}
+
+
+
+	public boolean isCancellato() {
+		return isCancellato;
+	}
+
+
+
+	public void setCancellato(boolean isCancellato) {
+		this.isCancellato = isCancellato;
+	}
+    
+}
---
+++ src/main/java/model/Utente.java
@@ -0,0 +1,90 @@
+package model;
+
+import java.io.Serializable;
+
+public class Utente implements Serializable{
+	
+	private int idUtente;
+	private String nome;
+	private String cognome;
+	private String email;
+	private String password;
+	private String indirizzo;
+	private boolean admin;
+	
+	public Utente() {
+		
+	}
+
+	public Utente(int idUtente, String nome, String cognome, String email, String password, String indirizzo,
+			boolean admin) {
+		super();
+		this.idUtente = idUtente;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.password = password;
+		this.indirizzo = indirizzo;
+		this.admin = admin;
+	}
+
+
+
+	public int getIdUtente() {
+		return idUtente;
+	}
+
+	public void setIdUtente(int idUtente) {
+		this.idUtente = idUtente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
+	
+}
